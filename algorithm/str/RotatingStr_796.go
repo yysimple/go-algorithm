77bb38913927e2edd4d6a56a8f89a911218b7b07
wuchengxing <1449697757@qq.com>
2021-08-14 00:51:43 +0800

    feat：旋转字符串，lc 796

--- algorithm/str/RotatingStr_796.go
+++ algorithm/str/RotatingStr_796.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 /**
 旋转字符串，lc 796
 */
 func main() {
-	str := "abcde"
+	str1 := "abcde"
+	str2 := "cdeab"
+	str := rotatingStr(str1, str2)
 	fmt.Println("str =", str)
+
+}
+
+/**
+旋转数组
+*/
+func rotatingStr(fatherStr string, sonStr string) bool {
+	fatherStr = fatherStr + fatherStr
+	return strings.Contains(fatherStr, sonStr)
 }
