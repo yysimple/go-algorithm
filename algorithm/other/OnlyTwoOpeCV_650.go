2896a049ea49d7fd24b4d66dc62e2d19aceefe91
wuchengxing <1449697757@qq.com>
2021-09-14 21:36:43 +0800

    feat：只有两个键的键盘, lc 650

--- algorithm/other/OnlyTwoOpeCV_650.go
+++ algorithm/other/OnlyTwoOpeCV_650.go
@@ -4,13 +4,15 @@ import "fmt"
 
 /**
 只有两个键的键盘, lc 650
-
 */
 func main() {
-	steps := minSteps(10)
+	steps := minSteps(30)
 	fmt.Println("steps =", steps)
 }
 
+/**
+这里其实求的就是素素相加就拆分一下30 = 2 * 3 * 5，都是素数，然后在相加 2 + 3 + 5 = 10
+*/
 func minSteps(n int) int {
 	res := 0
 	for i := 2; i <= n; i++ {
